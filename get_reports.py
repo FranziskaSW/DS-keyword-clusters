df3f5e74a807209132e40522cc9b0dcda2ece66c
FranziskaSW <franziska.wehrmann@gmail.com>
2019-01-30 17:07:02 +0200

    packages don't work, write own functions to get articles

--- get_reports.py
+++ get_reports.py
@@ -1,2 +1,38 @@
 import pandas as pd
 import json
+import requests
+from bs4 import BeautifulSoup
+
+our_key = ''
+keyword = 'election'
+page = 1
+
+# URL
+url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?' + 'q=' + keyword + '&page=' + str(page) + '&api-key=' + our_key
+
+print('-------------- load', url, ' --------------')
+html = requests.get(url)  # load page
+a = html.text
+api_return = json.loads(a)
+response = api_return['response']
+meta = response['meta']
+articles = response['docs']
+
+article = articles[2]
+article['_id']
+article['pub_date']
+article['headline']
+article['byline']
+article['keywords']
+article['source']
+article['document_type']
+article['word_count']
+article['uri']
+article['type_of_material']
+article['news_desk']
+article['score']
+article['snippet']
+article['blog']
+article['section_name']
+article['web_url']
+article['multimedia']
